3574428cbc4fe21db984a7ceb8730bbcb17a9989
FrancoisJohannson <erhard.janak@t-online.de>
2019-04-30 22:32:37 +0200

    all verbs async

--- code.js
+++ code.js
@@ -3,7 +3,7 @@
 var ContextRoot = "http://localhost:8080/members";
 
 
-function request(url) {
+function request(verb,url,body) {
     return new Promise(function(resolve, reject) {
       const xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function(e) {
@@ -13,7 +13,6 @@ function request(url) {
             document.getElementById("p1").innerHTML = "status:"+xhr.status+" ,response="+xhr.responseText;
           } else {
             reject(xhr.status);
-            //document.getElementById("p1").innerHTML = "status:"+xhr.status;
             document.getElementById("p1").innerHTML = "status:"+xhr.status+" ,response="+xhr.responseText;
           }
           
@@ -22,58 +21,34 @@ function request(url) {
       xhr.ontimeout = function () {
         reject('timeout');
       }
-      xhr.open('GET', url, true);
+      xhr.open(verb, url, true);
+
       xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send();
+      
+      if (body ) {
+        xhr.send(body);
+      }
+      else {
+        xhr.send();
+      }
+
     })
   }
 
 
-function createRequest() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        
-        if (this.readyState == 4 ) {
-            if ( this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            var response = xhttp.responseText;            
-            }
-            document.getElementById("p1").innerHTML = "status:"+this.status+" ,response="+xhttp.responseText;
-        }
-    };
-
-    return xhttp;
-}
-
-/*
-function Get(uri) {
-    var xhttp = createRequest();
-    xhttp.open("GET", uri, true);
-    xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send();  
-}
-*/
-
 // POST is for insterting a new element
-function PostMember() {
-    var xhttp = createRequest();
-    xhttp.open("POST", ContextRoot, true);
-    xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send('{"id":7,"surname":"Merkel","name":"Angela"}');  
+async function PostMember() {
+    const r = await request('POST',ContextRoot,'{"id":8,"surname":"Turner","name":"Tina"}');
 }
 
 // PUT is for changeing an element
-function PutMember() {
-    var xhttp = createRequest();
-    xhttp.open("PUT", ContextRoot, true);
-    xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send('{"id":1,"surname":"Curie","name":"Marie"}');  
+async function PutMember() {
+    const r = await request('PUT',ContextRoot,'{"id":1,"surname":"Curie","name":"Marie2"}');
 }
 
 
 async function GetAllMembers() {
-    //Get(ContextRoot);
-    const r = await request(ContextRoot);
+    const r = await request('GET',ContextRoot);
     var as = JSON.parse(r);
 
     for (var i = 0; i < as.length; i++) {
@@ -85,7 +60,5 @@ async function GetAllMembers() {
 }
 
 async function GetMember() {
-    const r = await request(ContextRoot.concat("/1"));
-    //Get(ContextRoot.concat("/1"));
-
+    const r = await request('GET',ContextRoot.concat("/1"));
 }
