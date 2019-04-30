4ad6a44d66c5cfa339fce65a247ab1bc66b30bb2
FrancoisJohannson <erhard.janak@t-online.de>
2019-04-30 12:36:59 +0200

    common function Get

--- code.js
+++ code.js
@@ -1,5 +1,4 @@
-function GetAllMembers() {
-
+function Get(uri) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         console.log("readyState:"+this.readyState);
@@ -11,25 +10,16 @@ function GetAllMembers() {
             alert("response:"+response);
         }
     };
-    xhttp.open("GET", "http://localhost:8080/members", true);
+    xhttp.open("GET", uri, true);
     xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send();
+    xhttp.send();  
+}
+
+function GetAllMembers() {
+    Get("http://localhost:8080/members");
 }
 
 function GetMember() {
+    Get("http://localhost:8080/members/1");
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        console.log("readyState:"+this.readyState);
-        console.log("status:"+this.status);
-        if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            var response = xhttp.responseText;
-            console.log("ok"+response);
-            alert("response:"+response);
-        }
-    };
-    xhttp.open("GET", "http://localhost:8080/members/1", true);
-    xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send();
 }
