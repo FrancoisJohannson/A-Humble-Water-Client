cb041ad4428c6eb630006fdf1cf113b4770c2863
FrancoisJohannson <erhard.janak@t-online.de>
2019-04-30 12:51:21 +0200

    POST Member

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function Get(uri) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -13,6 +15,23 @@ function Get(uri) {
     xhttp.open("GET", uri, true);
     xhttp.setRequestHeader("Content-Type", "application/json");
     xhttp.send();  
+}
+
+function PostMember() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        console.log("readyState:"+this.readyState);
+        console.log("status:"+this.status);
+        if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            var response = xhttp.responseText;
+            console.log("ok"+response);
+            alert("response:"+response);
+        }
+    };
+    xhttp.open("POST", "http://localhost:8080/members", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send('{"id":5,"surname":"Frida","name":"Kahlo"}');  
 }
 
 function GetAllMembers() {
