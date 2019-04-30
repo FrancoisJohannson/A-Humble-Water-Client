3716b52fdeca6db7732fdd961525ca7d55f6d02e
FrancoisJohannson <erhard.janak@t-online.de>
2019-04-30 13:04:10 +0200

    createRequest

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 "use strict";
 
-function Get(uri) {
+
+function createRequest() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         console.log("readyState:"+this.readyState);
@@ -12,26 +13,23 @@ function Get(uri) {
             alert("response:"+response);
         }
     };
+
+    return xhttp;
+}
+
+
+function Get(uri) {
+    var xhttp = createRequest();
     xhttp.open("GET", uri, true);
     xhttp.setRequestHeader("Content-Type", "application/json");
     xhttp.send();  
 }
 
 function PostMember() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        console.log("readyState:"+this.readyState);
-        console.log("status:"+this.status);
-        if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            var response = xhttp.responseText;
-            console.log("ok"+response);
-            alert("response:"+response);
-        }
-    };
+    var xhttp = createRequest();
     xhttp.open("POST", "http://localhost:8080/members", true);
     xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send('{"id":5,"surname":"Frida","name":"Kahlo"}');  
+    xhttp.send('{"id":6,"surname":"Gulbrannson","name":"Olaf"}');  
 }
 
 function GetAllMembers() {
